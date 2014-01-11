f2f6745d63bf16cc9db1a0001db02cbf74cb00ad
vagrant <vagrant@rails-dev-box>
2014-01-11 22:45:21 +0000

    setup git for odot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = '849e3008a690df818e199932afc4af866cdecf820ee8856654a99bf38662f253bd2f1ceff0fde469256f9400181a3de3ddfb182ae1feb63dbca5db034d71381f'
